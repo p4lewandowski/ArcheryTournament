593a740372a737ba88f375d52e9f861697f074c8
p4lewandowski <p4lewandowski@gmail.com>
2018-10-28 11:28:13 +0100

    Json can be sent properly on the localhost with CORS enabled

--- ArcheryTournament/views.py
+++ ArcheryTournament/views.py
@@ -6,4 +6,4 @@ from django.http import JsonResponse
 def json_pass(request):
 
     # return HttpResponse(json.dumps({'message': 'Mam nadzieje, ze teraz zadziala', 'nonie':'serio'}), content_type='application/json')
-    return JsonResponse({'message': 'Mam nadzieje, ze teraz zadziala', 'nonie':'serio'})
+    return JsonResponse({'message': 'Kuwa działa xDDD', 'nonie':'serio'})
--- ArcheryTournament/wsgi.py
+++ ArcheryTournament/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ArcheryTorunament project.
+WSGI config for ArcheryTournament project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArcheryTorunament.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArcheryTournament.settings')
 
 application = get_wsgi_application()
